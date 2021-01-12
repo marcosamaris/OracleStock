3a4f316231824210cd1b68f3f37404efe56d1178
marcosamaris <marcos.amaris@gmail.com>
2021-01-12 17:42:05 -0300

    Uploaded code about constants tweeter access authentication

---
+++ src/constants_tweet.py
@@ -0,0 +1,10 @@
+#Get started with Twitter APIs and tools
+#Apply for access https://developer.twitter.com/en/apply-for-access
+
+
+consumer_key = 'TYPE HERE YOUR TWEETER CONSUMER KEY'
+consumer_secret = 'TYPE HERE YOUR TWEETER CONSUMER SECRET'
+access_token = 'TYPE HERE YOUR TWEETER ACCESS TOKEN'
+access_token_secret = 'TYPE HERE YOUR TWEETER ACCESS TOKEN SECRET'
+
+num_of_tweets = int(1000)
