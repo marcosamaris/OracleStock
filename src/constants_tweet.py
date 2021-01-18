f2b34641a2a233ea08bba2a1a8eb8355ed11291a
Marcos Amarís González <marcos.amaris@gmail.com>
2021-01-18 11:40:13 -0300

    Update constants_tweet.py

--- src/constants_tweet.py
+++ src/constants_tweet.py
@@ -1,6 +1,6 @@
-consumer_key = 'lsIoq5RQF3gAbyePBjqY9OmVH'
-consumer_secret = 'Jv6djJnDGBYUhkmPCSHbnkp3PNNVv3pS9Y8ZRK7EaNEy96Qwbc'
-access_token = '81228854-VrCQYw7N2xPB4YFbYssCR0ghni1NsPF7coEcp5yBE'
-access_token_secret = 'oiRxql6q1xUiIEdU9wLwIv4GWMtDIyY5NiX0R6yy5bfCu'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 num_of_tweets = int(1000)
