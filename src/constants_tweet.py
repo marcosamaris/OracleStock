68727984ac8ffe341536e49a09e53a310c1a3275
marcosamaris <marcos.amaris@gmail.com>
2021-01-30 17:46:35 -0300

    Uploaded constants_tweet code

---
+++ src/constants_tweet.py
@@ -0,0 +1,6 @@
+consumer_key = 'TYPE HERE YOUR TWEETER CONSUMER KEY'
+consumer_secret = 'TYPE HERE YOUR TWEETER CONSUMER SECRET'
+access_token = 'TYPE HERE YOUR TWEETER ACCESS TOKEN'
+access_token_secret = 'TYPE HERE YOUR TWEETER ACCESS TOKEN SECRET'
+
+num_of_tweets = int(1000) 
